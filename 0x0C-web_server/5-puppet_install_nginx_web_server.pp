55acfade5575c1f1d4cc25d56a29a2e3b5c8f6b2
Nightsee <younes.naitsi.2002@gmail.com>
2023-10-05 11:00:59 -0700

    fix

--- 0x0C-web_server/5-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/5-puppet_install_nginx_web_server.pp
@@ -49,7 +49,7 @@ file { 'Nginx default config file':
                root /var/www/html;
         # Add index.php to the list if you are using PHP
         index index.html index.htm index.nginx-debian.html;
-        server_name _;
+        server_name nightsee.tech;
         location / {
                 # First attempt to serve request as file, then
                 # as directory, then fall back to displaying a 404.
